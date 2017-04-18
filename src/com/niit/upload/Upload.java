9d61946851c865e2d047f586473cafebe967474d
Sourav Kumar Biswas <souravictory@gmail.com>
2017-04-18 12:42:04 +0530

    Finished with chnages

--- src/com/niit/upload/Upload.java
+++ src/com/niit/upload/Upload.java
@@ -6,37 +6,32 @@ public class Upload {
 	
 	    public static void main(String[]args) throws IOException
 	    {
-	        int length,number, i, j = 0;
-	        int arr[][] = new int[2][2];
+	        int length,number, i, j ;
+	        
 	        Scanner scan = new Scanner(System.in);
 	        System.out.println("Enter the length ");
 	        length = scan.nextInt();
+	        
+	        
 	     do{
-	        	System.out.println("Enter number of photos to upload");
-		        number = scan.nextInt();
-		        System.out.println("Enter the dimension");
-	        for(i =0;i<2;i++){
-	            for(j=0;j<2;j++){
-	            	 
-	                arr[i][j] = scan.nextInt();
-	                if(arr[i][j]<length)
-	   	    	 {
-	   	    		 System.out.println("Upload another"+arr[i][j]); 
-	   	    	 }
-	                else if(arr[i][j]==length)
-	                {
-		   	    		 System.out.println("Accepted"+arr[i][j]); 
-		   	    	 }	
-	                else if(arr[i][j]>length)
-	                {
-		   	    		 System.out.print	("Crop it"+arr[i][j]); 
-		   	    	 }	
-	            }
-	        }
-	        
-	        
-	    }while(((number<3)&&(number>0)));
-	     scan.close();
-	}
+	    	 System.out.println("Enter the number of photos to upload ");
+		        number=scan.nextInt();	
+		        
+		        for(int h=1;h<=number;h++)
+		           {
+		        	System.out.println("Enter the length and breadth");
+		        i=scan.nextInt();
+		        j=scan.nextInt();
+	    	if((i<length || j<length) ||(i<length && j<length))
+	    		System.out.println("Upload another");
+	    	else if((i>length || j>length) ||(i>length && j>length))
+	    		System.out.println("Crop it");
+	    	else if((i==length && j==length))
+	    		System.out.println("Accepted");
+	    	else
+	    		System.out.println("Out of Range");
+		           }scan.close();
+	}while(!((number<10)&&(number>0)));
+} 
 }
 
